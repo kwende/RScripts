e0931ce17c26a561f6df884b0417a87f5eb92ed2
Ben Rush <ben@ben-rush.net>
2014-12-10 00:32:57 -0600

    experimenting with the dbinom function.

--- adam.r
+++ adam.r
@@ -1,3 +1,5 @@
+library(bbmle)
+
 #negative log likelihood function. monotonically increasing
 #and maximizing the likelihood function is equivalent 
 #in all ways to minimizing the negative log likelihood. 
@@ -7,3 +9,16 @@ binom = function(probability,results,numberOfTrials){
 
 #test data. this is to test for a normal Bernoulli coin-toss
 #trial style data-set. 
+testData = c(1,1,0,0,1,1,0,1,1,1,1,0,1)
+
+#for plotting
+xVec = 0:(length(testData)-1)
+yVec = dbinom(testData,prob=.5,size=length(testData),log=FALSE); 
+
+plot(xVec,yVec,xlab="X",ylab="Prob")
+
+#r = mle2(minuslogl = binom, 
+#     start = list(probability=.75), 
+#     data = list(numberOfTrials=length(testData), results = testData))
+
+#summary(r)
