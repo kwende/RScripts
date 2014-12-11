934fdf694d10b8c41f9328d08d9fcf4251b79eb9
kwende <ben@ben-rush.net>
2014-12-11 13:29:43 -0600

    a bit more work

--- adam2.r
+++ adam2.r
@@ -1,26 +1,52 @@
 library(bbmle)
 
 sigmoid = function(x,a,d,b,x0){
+  if(a < 0) a = 0; 
+  if(a > 1) a = 1; 
+  if(d < 0) d = 0; 
+  if(d > 1) d = 1; 
+  if(b < 0) b = 0; 
+  
   ret = ((a-d)*(1+exp(-b*x0)))/(1+exp(b*(x-x0))) + d
+
+  if(ret < 0){
+    print(c(a,d,b,x0));
+    ret = 0; 
+  }
+  if(ret > 1){
+    print(c(a,d,b,x0));
+    ret = 1; 
+  }
+  
   return(ret)
 }
 
-probability = function(x,a1,b1,x01,d2,b2,x02){
-  ret = 1 * sigmoid(x,a1,1,b1,x01) * sigmoid(x,1,d2,b2,x02); 
+PM3 = function(x,a1,b1,x01,d2,b2,x02,c){
+  if(x01 >= x02) return(0)
+  
+  ret = c * sigmoid(x,a1,1,b1,x01) * sigmoid(x,1,d2,b2,x02); 
   return(ret)
 }
 
-likelihood = function(values,data,a1,b1,x01,d2,b2,x02){
+likelihood = function(values,data,a1,b1,x01,d2,b2,x02,c){
+
   sum = 0
   for(i in 1:length(values)){
     
     environ = data[i]; 
     isFound = values[i]
-    prob = probability(environ,a1,b1,x01,d2,b2,x02);
+    
+    prob = PM3(environ,a1,b1,x01,d2,b2,x02,c);
     val = 0;
-    if(prob!=0){
-      val = -log10(prob^isFound * (1-prob))
+    
+    if(prob>0 && prob<1){
+      val = -log10(prob^isFound * (1-prob)^(1-isFound))
     }
+    else{
+      val = 10000000000
+      #print(c(a1,d2,b1,b2,x01,x02,c));
+    }
+    
     sum = sum + val
   }
 
@@ -29,24 +55,41 @@ likelihood = function(values,data,a1,b1,x01,d2,b2,x02){
 
 csv = read.csv(file="thresholds.csv",head=TRUE,sep=",")
 
-v = csv[,1]
-d = csv[,12]
+#v = csv[,1]
+#d = csv[,17]
+d = z
+v = x
 
 r = mle2(minuslogl = likelihood, 
-         start = list(x01=0.5,x02=0.5, a1=.5, b1=1, d2=.5, b2=1), 
-         data = list(values=v,data=d),
-         lower = c(a1=0.001,b1=0.001,d2=0.001,b2=0.001),
-         upper = c(a1=1,d2=1), 
-         method="L-BFGS-B")
+        start = list(x01=20,x02=50, a1=.5, b1=1, d2=.5, b2=1, c=.5), 
+        data = list(values=v,data=d),
+        lower = c(x01=-10,x02=26, a1=0, b1=0, d2=0, b2=0, c=0),
+        upper = c(x01=25, x02=75, a1=1, b1=1, d2=1, b2=1, c=1),
+        method="L-BFGS-B")
 
 print(r)
 
 x = 0:100
 y = 0:100
 for(i in 0:101){
-  y[i] = probability(i, r@coef["a1"], r@coef["b1"], r@coef["x01"], 
-                     r@coef["d2"], r@coef["b2"], r@coef["x02"])
+  
+  a1 = r@coef["a1"]
+  b1 = r@coef["b1"]
+  x01 = r@coef["x01"]
+  d2 = r@coef["d2"]
+  b2 = r@coef["b2"]
+  x02 = r@coef["x02"]
+  c = r@coef["c"]  
+  
+  #a1 = .3
+  #b1 = .3
+  #x01 = 20
+  #d2 = 0
+  #b2 = .1
+  #x02 = 50
+  #c = 1
+  
+  y[i] = PM3(i, a1, b1, x01, d2, b2, x02, c)
 }
 
-plot(range(x), range(y), xlab="X",ylab="Prob", type="n")
-lines(x,y,type="b")
+plot(x, y, xlab="X",ylab="Prob", type="o")
