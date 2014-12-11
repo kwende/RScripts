ee57e2c3537c421b9cf4cec492510bee0a517644
Ben Rush <ben@ben-rush.net>
2014-12-11 00:09:49 -0600

    got the gradient descent working, but now need to interpret the results and make sure it's working correctly

--- adam2.r
+++ adam2.r
@@ -1,10 +1,7 @@
 library(bbmle)
 
 sigmoid = function(x,a,d,b,x0){
-  ret = ((a-d)*(1+exp(-b*x0)))/(1+exp(b*(x-x0)))
-  if(ret < 0){
-    print(c("Sigmoid error: ",c(x,a,d,b,x0)))
-  }
+  ret = ((a-d)*(1+exp(-b*x0)))/(1+exp(b*(x-x0))) + d
   return(ret)
 }
 
@@ -13,30 +10,31 @@ probability = function(x,a1,b1,x01,d2,b2,x02,c){
 }
 
 likelihood = function(values,data,a1,b1,x01,d2,b2,x02,c){
-  
   sum = 0
   for(i in 1:length(values)){
     
     environ = data[i]; 
     isFound = values[i]
-    prob = probability(environ,a1,b1,x01,d2,b2,x02,c); 
-    val = -log10(prob^isFound * (1-prob))
-    #if(is.nan(val)){
-    #  print(c("ERROR: isFound:",isFound,", prob: ", prob))
-    #}
+    prob = probability(environ,a1,b1,x01,d2,b2,x02,c);
+    val = 0;
+    if(prob!=0){
+      val = -log10(prob^isFound * (1-prob))
+    }
     sum = sum + val
   }
   return(sum)
 }
 
-csv = read.csv(file="C:/Users/brush/Downloads/Threshold modelling.csv",head=TRUE,sep=",")
+csv = read.csv(file="thresholds.csv",head=TRUE,sep=",")
 
 v = csv[,1]
 d = csv[,12]
 
-#print(probability(1,0,0,0,0,0,0,1))
-#print(likelihood(v,d,0,0,0,0,0,0,1))
-
 r = mle2(minuslogl = likelihood, 
-         start = list(a1=0.1,b1=0.1,x01=0.1,d2=0.1,b2=0.1,x02=0.1,c=1), 
-         data = list(values=v,data=d))
+         start = list(x01=0.1,x02=0.1, c=1, a1=.1, b1=.1, d2=.1, b2=.1), 
+         data = list(values=v,data=d),
+         lower = c(a1=0,b1=0,d2=0,b2=0),
+         upper = c(a1=1,b1=1,d2=1,b2=1), 
+         method="L-BFGS-B")
+
+print(r)
