e782e5de0487fc808782a7f6ac67c1f5fb73cdde
kwende <ben@ben-rush.net>
2014-12-12 13:30:57 -0600

    put in PM2

--- adam2.r
+++ adam2.r
@@ -28,7 +28,13 @@ PM3 = function(x,a1,b1,x01,d2,b2,x02,c){
   return(ret)
 }
 
-likelihood = function(values,data,a1,b1,x01,d2,b2,x02,c){
+PM2 = function(x,a,d,b,x0){
+  
+  ret = sigmoid(x,a,d,b,x0)
+  return(ret);
+}
+
+PM3Likelihood = function(values,data,a1,b1,x01,d2,b2,x02,c){
   
   sum = 0
   for(i in 1:length(values)){
@@ -52,36 +58,88 @@ likelihood = function(values,data,a1,b1,x01,d2,b2,x02,c){
   return(sum)
 }
 
-csv = read.csv(file="thresholds2.csv",head=TRUE,sep=",")
+PM2Likelihood = function(values,data,a,d,b,x0){
+  sum = 0; 
+  
+  for(i in 1:length(values)){
+    environ = data[i]; 
+    isFound = values[i]; 
+    
+    prob = PM2(environ,a,d,b,x0); 
+    val = 0;
+    
+    if(prob > 0 && prob < 1){
+      val = -log10(prob^isFound * (1-prob)^(1-isFound));
+    }
+    else{
+      val = 10000000000
+    }
+    
+    sum = sum + val; 
+  }
+  
+  return(sum); 
+}
+
+PM3MLE = function(v,d){
+  leftHandInflectionSlope = .3 #b1
+  rightHandInfectionSlope = .3 #b2
+  leftHandAsymptote = 0 #a1
+  rightHandAsymptote = 0 #d2
+  leftHandInflectionPoint = 5 #x01
+  rightHandInflectionPoint = 15 #x02
+  peak = 1
+  
+  leftHandInflectionMin = 1
+  leftHandInflectionMax = 9
+  rightHandInflectionMin = 10
+  rightHandInflectionMax = 20
+  
+  r = mle2(minuslogl = PM3Likelihood, 
+           start = list(x01=leftHandInflectionPoint,
+                        x02=rightHandInflectionPoint, 
+                        a1=leftHandAsymptote, 
+                        b1=leftHandInflectionSlope, 
+                        d2=rightHandAsymptote, 
+                        b2=rightHandInfectionSlope, 
+                        c=peak), 
+           data = list(values=v,data=d),
+           lower = c(x01=leftHandInflectionMin,x02=rightHandInflectionMin, a1=0, b1=0, d2=0, b2=0, c=0),
+           upper = c(x01=leftHandInflectionMax, x02=rightHandInflectionMax, a1=1, b1=1, d2=1, b2=1, c=1),
+           method="L-BFGS-B")
+  
+  return(r);
+}
+
+PM2MLE = function(v,d){
+  inflectionPointSlope = .3 #b
+  leftHandAsymptote = 0 #a
+  rightHandAsymptote = 0 #d
+  inflectionPoint = 20 #x0
+  
+  inflectionPointMin = 1
+  inflectionPointMax = 100
+  
+  r = mle2(minuslogl = PM2Likelihood, 
+           start = list(x0=inflectionPoint,
+                        a=leftHandAsymptote, 
+                        b=inflectionPointSlope, 
+                        d=rightHandAsymptote), 
+           data = list(values=v,data=d),
+           lower = c(x0=inflectionPointMin, a=0, b=0, d=0),
+           upper = c(x0=inflectionPointMax, a=1, b=1, d=1),
+           method="L-BFGS-B")
+  
+  return(r);  
+}
+
+csv = read.csv(file="thresholds.csv",head=TRUE,sep=",")
 
 v = csv[,1]
 d = csv[,2]
 
-leftHandInflectionSlope = .3 #b1
-rightHandInfectionSlope = .3 #b2
-leftHandAsymptote = 0 #a1
-rightHandAsymptote = 0 #d2
-leftHandInflectionPoint = 10 #x01
-rightHandInflectionPoint = 50 #x02
-peak = 1
-
-leftHandInflectionMin = 1
-leftHandInflectionMax = 25
-rightHandInflectionMin = 26
-rightHandInflectionMax = 75
-
-r = mle2(minuslogl = likelihood, 
-        start = list(x01=leftHandInflectionPoint,
-                     x02=rightHandInflectionPoint, 
-                     a1=leftHandAsymptote, 
-                     b1=leftHandInflectionSlope, 
-                     d2=rightHandAsymptote, 
-                     b2=rightHandInfectionSlope, 
-                     c=peak), 
-        data = list(values=v,data=d),
-        lower = c(x01=leftHandInflectionMin,x02=rightHandInflectionMin, a1=0, b1=0, d2=0, b2=0, c=0),
-        upper = c(x01=leftHandInflectionMax, x02=rightHandInflectionMax, a1=1, b1=1, d2=1, b2=1, c=1),
-        method="L-BFGS-B")
+r = PM3MLE(v,d);
+#r = PM2MLE(v,d);
 
 print(r)
 
@@ -97,6 +155,11 @@ for(i in 0:101){
   x02 = r@coef["x02"]
   c = r@coef["c"]  
   
+  #a = r@coef["a"]; 
+  #b = r@coef["b"]; 
+  #d = r@coef["d"];
+  #x0 = r@coef["x0"];
+  
   #a1 = .1
   #b1 = .3
   #x01 = 20
@@ -105,7 +168,9 @@ for(i in 0:101){
   #x02 = 50
   #c = 1
   
+  #(x,a,d,b,x0)
   y[i] = PM3(i, a1, b1, x01, d2, b2, x02, c)
+  #y[i] = PM2(i,a,d,b,x0)
 }
 
 plot(x, y, xlab="X",ylab="Prob", type="o")
